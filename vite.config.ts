0929d85b2491a4500298faae95d12cdbdf1bd866
Purna-Ajay <purnaajay.in@bluecloudsoftech.com>
2025-11-16 11:58:36 +0530

    port change

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: true,     // exposes network IP automatically (192.x / 172.x)
     port: 8080,
-    open: "http://192.168.1.5:8080",
+    // open: "http://192.168.1.5:8080",
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
